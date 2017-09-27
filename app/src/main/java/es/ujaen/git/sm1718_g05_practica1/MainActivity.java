e3c40a40ef89087a1f51155da3ccafdcafee5424
AntonioOsuna97 <aom00033@red.ujaen.es>
2017-09-27 12:23:42 +0200

    Añadido botón login al fragmento de login Sesion 1. Punto 1 y 2 completados Sesion 2. Punto 5 completado Falta 3 y 4 para casa

--- app/src/main/java/es/ujaen/git/sm1718_g05_practica1/MainActivity.java
+++ app/src/main/java/es/ujaen/git/sm1718_g05_practica1/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //Añado fragmento
+       setContentView(R.layout.activity_main);
+
+
     }
 }
